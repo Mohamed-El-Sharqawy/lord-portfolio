d74b5971085be983734eec996dc8851e8b28c0f8
Mohamed Ahmed <dev.elbehery@gmail.com>
2024-10-19 23:36:06 +0300

    Update page.tsx
    remove any unused imports from page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,3 @@
-import Hero from "./_components/Hero/Hero";
-import Logos from "./_components/Logos/Logos";
-import Services from "./_components/Services/Services";
-
 export default function Home() {
   return (
     <>
